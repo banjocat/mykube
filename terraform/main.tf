61af5e5cbaf83a10fa406c0758c46c279d38ae1c
banjocat <jackmuratore@gmail.com>
2017-08-09 23:40:54 -0400

    Add terraform and kubespray

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+variable "do_token" {}
+
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "master" {
+    image = "ubuntu-16-04-x64"
+    name = "kube-master-1"
+    region = "nyc1"
+    size = "512mb"
+    ssh_keys = ["ce:1b:52:47:94:e7:f5:43:dc:a7:6e:ac:5d:3e:d2:6d"]
+}
+
+resource "digitalocean_droplet" "nodes" {
+    image = "ubuntu-16-04-x64"
+    name = "kube-node-1"
+    region = "nyc1"
+    size = "512mb"
+    ssh_keys = ["ce:1b:52:47:94:e7:f5:43:dc:a7:6e:ac:5d:3e:d2:6d"]
+}
+
+resource "digitalocean_floating_ip" "master_ip" {
+    droplet_id = "${digitalocean_droplet.master.id}"
+    region = "${digitalocean_droplet.master.region}"
+}
+
+output "master-1" {
+    value = "${digitalocean_droplet.master.ipv4_address}"
+}
+
+output "nodes-1" {
+    value = "${digitalocean_droplet.nodes.ipv4_address}"
+}
