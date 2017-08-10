a01cae9c3fba6f5c8daab92def1632cb9a2f4f61
banjocat <jackmuratore@gmail.com>
2017-08-09 23:45:03 -0400

    Use counts for master and nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,18 +6,20 @@ provider "digitalocean" {
 
 resource "digitalocean_droplet" "master" {
     image = "ubuntu-16-04-x64"
-    name = "kube-master-1"
+    name = "kube-master-${count.index + 1}"
     region = "nyc1"
     size = "512mb"
     ssh_keys = ["ce:1b:52:47:94:e7:f5:43:dc:a7:6e:ac:5d:3e:d2:6d"]
+    count = 1
 }
 
 resource "digitalocean_droplet" "nodes" {
     image = "ubuntu-16-04-x64"
-    name = "kube-node-1"
+    name = "kube-node-${count.index + 1}"
     region = "nyc1"
     size = "512mb"
     ssh_keys = ["ce:1b:52:47:94:e7:f5:43:dc:a7:6e:ac:5d:3e:d2:6d"]
+    count = 1
 }
 
 resource "digitalocean_floating_ip" "master_ip" {
@@ -25,10 +27,10 @@ resource "digitalocean_floating_ip" "master_ip" {
     region = "${digitalocean_droplet.master.region}"
 }
 
-output "master-1" {
-    value = "${digitalocean_droplet.master.ipv4_address}"
+output "master" {
+    value = "${digitalocean_droplet.master.*.ipv4_address}"
 }
 
-output "nodes-1" {
-    value = "${digitalocean_droplet.nodes.ipv4_address}"
+output "nodes" {
+    value = "${digitalocean_droplet.nodes.*.ipv4_address}"
 }
